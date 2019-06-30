2fd47c9ac39afe4f52b6a6b0b7e606ee339959e0
connorguerin <connorguerin239@gmail.com>
2019-06-30 18:39:30 -0400

    Create AllocationAlgorithms.h

---
+++ algorithms/AllocationAlgorithms.h
@@ -0,0 +1,10 @@
+//
+// Created by Connor on 2/22/2019.
+//
+
+#ifndef UNTITLED_ALLOCATIONALGORITHMS_H
+#define UNTITLED_ALLOCATIONALGORITHMS_H
+int bestFit(int size, void *list);
+int worstFit(int size, void *list);
+#endif //UNTITLED_ALLOCATIONALGORITHMS_H
+
